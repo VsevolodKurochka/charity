b047096b719d21fa5c9724c281078805fc7c579b
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-06-22 12:05:12 +0300

    Fixed contrast section in Front page

--- static/src/js/jquery.main.js
+++ static/src/js/jquery.main.js
@@ -11,43 +11,6 @@ $(document).ready(function(){
 		scroll($( $(this).attr('href') ), 1500);
 	});
 
-	// Collapse
-
-		$(".js-collapse__group.active").children(".js-collapse__group-body").slideDown();
-
-		$('.js-collapse').on('click', '.js-collapse__group-header', function(){
-			var collapseInner = $(this).parents('.js-collapse').find('.js-collapse__group');
-
-			$(this)
-				.parent()
-				.toggleClass('active');
-
-			$(this)
-				.next()
-				.slideToggle('slow');
-
-			collapseInner
-				.not($(this).parent())
-				.removeClass('active');
-
-			collapseInner
-				.children('.js-collapse__group-body')
-				.not($(this).next())
-				.slideUp("slow");
-
-		});
-	// Tabs
-		$('[data-action="tab"]').click(function(){			
-			// Tab links toggle class
-				$(this).closest(".vtabs__list").children("li").removeClass('active');
-				$(this).parent().addClass('active');
-			// Show tab content
-				var tabTarget = $(this).attr('data-target');
-				$(tabTarget).fadeIn('slow');
-				$(".vtabs__content > div").not($(tabTarget)).fadeOut('slow');
-		});
-	// Develope
-
 	$('#motivation-slider').owlCarousel({
     loop:true,
     margin: 0,
@@ -59,5 +22,23 @@ $(document).ready(function(){
     autoplay: true,
     autoplayTimeout: 3000
 	});
+
+	// Page "Front"
+
+		$('.js-front-contrast-button').click(function () {
+			var toggle = $(this).attr('data-toggle');
+			var textOpen = $(this).attr('data-open-text');
+			var textClose = $(this).attr('data-close-text');
+
+			$("#" + toggle).slideToggle();
+
+			$(this).text() == textOpen ? $(this).text(textClose) : $(this).text(textOpen);
+
+			$('html, body').animate({
+				scrollTop: $("#" + toggle).offset().top - $('#js-navigation').height() - 15
+			}, 500);
+			return false;
+
+		});
 
 });	
